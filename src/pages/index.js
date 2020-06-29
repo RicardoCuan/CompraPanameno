009f1869ff824c8dccda643cb1375a88b74f21df
Ricardo Cuan <ricardo.cuan@aiesec.net>
2020-06-28 23:18:24 -0500

    Añadida primeras dependencias, primeros cambios al gatsby starter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Router } from '@reach/router'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Home from '../pages/Home'
+import NotFound from '../pages/NotFound'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  <Router>
+    <NotFound default /> 
+    <Home path='/' />
+  </Router>
 )
 
 export default IndexPage
